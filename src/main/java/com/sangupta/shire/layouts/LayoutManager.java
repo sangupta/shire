13c041157be73f1c9eeb0465fb313cca29694362
Sandeep Gupta <sandy.pec@gmail.com>
2012-08-21 14:23:34 +0530

    Modified so that layouts that extend other layouts can be used as well.

--- src/main/java/com/sangupta/shire/layouts/LayoutManager.java
+++ src/main/java/com/sangupta/shire/layouts/LayoutManager.java
@@ -22,8 +22,10 @@
 package com.sangupta.shire.layouts;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 import org.apache.commons.lang3.StringUtils;
 
@@ -31,7 +33,9 @@ import com.sangupta.makeup.Makeup;
 import com.sangupta.makeup.layouts.Layout;
 import com.sangupta.makeup.layouts.LayoutType;
 import com.sangupta.shire.ExecutionOptions;
+import com.sangupta.shire.domain.RenderableResource;
 import com.sangupta.shire.model.TemplateData;
+import com.sangupta.shire.util.FrontMatterUtils;
 import com.sangupta.shire.util.ShireUtils;
 
 /**
@@ -45,6 +49,17 @@ public class LayoutManager {
 	private ExecutionOptions options = null;
 	
 	private Layout layout = null;
+	
+	/**
+	 * Keeps track of which all templates are extended templates (with front-matter) so that
+	 * they can be parsed effectively.
+	 */
+	private final Map<String, Boolean> extendedTemplates = new HashMap<String, Boolean>();
+	
+	/**
+	 * Stores front-matter stored with layouts, if any
+	 */
+	private final Map<String, RenderableResource> renderableLayouts = new HashMap<String, RenderableResource>();
 	
 	public LayoutManager(ExecutionOptions options) {
 		this.options = options;
@@ -99,6 +114,68 @@ public class LayoutManager {
 	 * @return
 	 */
 	public String layoutContent(String layoutName, final String content, final TemplateData templateData) {
+		Boolean extended = this.extendedTemplates.get(layoutName);
+		if(extended == null) {
+			extended = decipherTemplateType(layoutName);
+		}
+		
+		if(!extended) {
+			// this is a simple template, only build the model and render
+			return putContentInTemplate(layoutName, content, templateData);
+		}
+		
+		// this is an extended template
+		// resolve the extension - build the entire template first
+		final RenderableResource resource = this.renderableLayouts.get(layoutName);
+		final Properties properties = resource.getFrontMatter();
+
+		String parentLayoutName = properties.getProperty("layout");
+		
+		// if the layout is null, only then check for default layout naming convention
+		// if the layout is empty - that indicates we do not want any layout applied
+		if(parentLayoutName == null) {
+			parentLayoutName = templateData.getSite().getDefaultLayoutName();
+		}
+		
+		// do as if we are in a normal template
+		if(parentLayoutName == null) {
+			return putContentInTemplate(layoutName, content, templateData);
+		}
+		
+		final Map<String, Object> dataModel = getDataModel(templateData);
+		
+		// we have the parent layout Name
+		// get the actual layout code
+		// get the code in recursion
+		String childTemplateCode = null;
+		try {
+			childTemplateCode = resource.getOriginalContent();
+		} catch (IOException e) {
+			// e.printStackTrace();
+		}
+		
+		String layoutCode = layoutContent(parentLayoutName, childTemplateCode, templateData);
+		
+		String modifiedContent;
+		if(content != null) {
+			modifiedContent = this.layout.layoutWithTemplateCode(layoutCode, dataModel);
+		} else {
+			modifiedContent = content;
+		}
+		
+		// layout the contents
+		dataModel.put("content", modifiedContent);
+		return this.layout.layoutWithTemplateCode(layoutCode, dataModel);
+	}
+	
+	/**
+	 * 
+	 * @param layoutName
+	 * @param content
+	 * @param templateData
+	 * @return
+	 */
+	private String putContentInTemplate(String layoutName, final String content, final TemplateData templateData) {
 		if(StringUtils.isEmpty(layoutName)) {
 			return content;
 		}
@@ -130,6 +207,52 @@ public class LayoutManager {
 		return layout.layout(layoutName, dataModel);
 	}
 
+	/**
+	 * Method that detects whether a template is extended or not. This is decided based on the presence
+	 * of front matter in the template.
+	 * 
+	 * @param layoutName
+	 * @return
+	 */
+	private Boolean decipherTemplateType(String layoutName) {
+		if(this.extendedTemplates.containsKey(layoutName)) {
+			return this.extendedTemplates.get(layoutName);
+		}
+		
+		final File layouts = ShireUtils.getFolder(options, options.getLayoutsFolderName());
+		final File includes = ShireUtils.getFolder(options, options.getIncludesFolderName());
+		
+		File layout = new File(layouts, layoutName);
+		
+		// check if it exists
+		// if not, try includes
+		if(!layout.exists()) {
+			layout = new File(includes, layoutName);
+		}
+		
+		if(!layout.exists()) {
+			// the layout does not exist in any of the given folders
+			throw new IllegalArgumentException("No layout exists in layouts/includes");
+		}
+		
+		Properties properties = new Properties();
+		try {
+			int frontMatter = FrontMatterUtils.checkFileHasFrontMatter(layout, properties);
+			if(frontMatter > 0) {
+				this.extendedTemplates.put(layoutName, true);
+				RenderableResource renderableResource = new RenderableResource(layout, layout.getAbsoluteFile().getParentFile().getAbsolutePath(), properties, frontMatter);
+				this.renderableLayouts.put(layoutName, renderableResource);
+				return true;
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		this.extendedTemplates.put(layoutName, false);
+		return false;
+	}
+	
 	/**
 	 * Create a data model out of this {@link TemplateData} object that can be
 	 * used with the {@link Layout}.
