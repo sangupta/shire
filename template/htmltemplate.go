30af7281f135ce3d76ae556936f5789689611f49
Sandeep Gupta <sandy.pec@gmail.com>
2022-08-02 14:32:04 -0700

    add copyright header

--- template/htmltemplate.go
+++ template/htmltemplate.go
@@ -1,3 +1,14 @@
+/**
+ * Shire
+ * https://github.com/sangupta/shire
+ *
+ * MIT License.
+ * Copyright (c) 2022, Sandeep Gupta.
+ *
+ * Use of this source code is governed by a MIT style license
+ * that can be found in LICENSE file in the code repository.
+ */
+
 package template
 
 import (
