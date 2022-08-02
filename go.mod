d4bae4e7ffd5f5772f5bd51268f83e4752867d3e
Sandeep Gupta <sandy.pec@gmail.com>
2022-08-02 14:29:50 -0700

    upgrade deps

--- go.mod
+++ go.mod
@@ -3,10 +3,8 @@ module github.com/sangupta/shire
 go 1.18
 
 require (
-	github.com/PuerkitoBio/goquery v1.8.0
 	github.com/deckarep/golang-set/v2 v2.1.0
-	github.com/sangupta/lhtml v0.1.0
-	golang.org/x/net v0.0.0-20220708220712-1185a9018129
+	github.com/sangupta/lhtml v0.2.1
 )
 
-require github.com/andybalholm/cascadia v1.3.1 // indirect
+require golang.org/x/net v0.0.0-20220708220712-1185a9018129 // indirect
--- template/htmltemplate.go
+++ template/htmltemplate.go
@@ -1,23 +1,19 @@
 package template
 
 import (
-	"bytes"
 	"errors"
 	"io/ioutil"
 	"path/filepath"
-	"strconv"
 
+	"github.com/sangupta/lhtml"
 	"github.com/sangupta/shire/logger"
-
-	"github.com/PuerkitoBio/goquery"
-	"golang.org/x/net/html"
 )
 
 //
 // Structure to implement ParsedTemplate interface.
 //
 type HtmlParsedTemplate struct {
-	Document *goquery.Document
+	Document *lhtml.HtmlElements
 }
 
 //
@@ -29,12 +25,6 @@ func parseHtmlTemplate(filePath string) ParsedTemplate {
 		panic(err)
 	}
 
-	l := doc.Find("page:title").Length()
-	logger.Debug("length: " + strconv.Itoa(l))
-
-	html, err := doc.Html()
-	logger.Debug(html)
-
 	// finally create an object for it
 	parsed := &HtmlParsedTemplate{
 		Document: doc,
@@ -43,18 +33,15 @@ func parseHtmlTemplate(filePath string) ParsedTemplate {
 	return parsed
 }
 
-func getDocument(filePath string) (*goquery.Document, error) {
+func getDocument(filePath string) (*lhtml.HtmlElements, error) {
 	logger.Debug("Reading template file: " + filePath)
 	fileBytes, err := ioutil.ReadFile(filePath)
 	if err != nil {
 		return nil, err
 	}
 
-	// create a reader
-	reader := bytes.NewReader(fileBytes)
-
 	// create html doc
-	doc, err := goquery.NewDocumentFromReader(reader)
+	doc, err := lhtml.ParseHtmlString(string(fileBytes))
 	if err != nil {
 		return nil, err
 	}
@@ -68,17 +55,17 @@ func getDocument(filePath string) (*goquery.Document, error) {
 //
 // Include all imports specified in this document
 //
-func includeImports(doc *goquery.Document, path string) {
+func includeImports(doc *lhtml.HtmlElements, path string) {
 	logger.Debug("Processing any includes in file: " + path)
-	selection := doc.Find("shireInclude")
-	if selection == nil || selection.Length() == 0 {
+	importNodes := doc.GetElementsByName("shireInclude")
+	if importNodes.Length() == 0 {
 		logger.Debug("No <shire:include> found in file: " + path)
 		return
 	}
 
 	// go over each file
-	for _, node := range selection.Nodes {
-		src := getAttribute(node, "src")
+	for _, importNode := range importNodes.Nodes() {
+		src := getAttribute(importNode, "src")
 		if src == "" {
 			logger.Warn("File specifies shire:import but does not have 'src' attribute: " + path)
 			continue
@@ -86,7 +73,7 @@ func includeImports(doc *goquery.Document, path string) {
 
 		// the include source was found
 		includePath := filepath.Join(filepath.Dir(path), src)
-		doc, err := getDocument(includePath)
+		importedChildDocument, err := getDocument(includePath)
 		// html, err := doc.Html()
 		// logger.Debug(html)
 		if err != nil {
@@ -94,17 +81,19 @@ func includeImports(doc *goquery.Document, path string) {
 		}
 
 		// replace
-		parent := node.Parent
+		parent := importNode.Parent()
 
-		body := doc.Find("body")
-		bodyChildren := body.Children()
+		importedBody := importedChildDocument.GetElementsByName("body")
+		if importedBody != nil && importedBody.Length() > 0 {
+			bodyChildren := importedBody.Get(0)
 
-		if bodyChildren.Length() > 0 {
-			for _, child := range bodyChildren.Nodes {
-				child.Parent.RemoveChild(child)
-				parent.InsertBefore(child, node)
+			if bodyChildren.NumChildren() > 0 {
+				for _, importedBodyChild := range bodyChildren.Children() {
+					parent.InsertBeforeChild(importNode, importedBodyChild)
+				}
+
+				importNode.RemoveMe()
 			}
-			parent.RemoveChild(node)
 		}
 	}
 }
@@ -112,10 +101,10 @@ func includeImports(doc *goquery.Document, path string) {
 //
 // For a given node, find the required attribute
 //
-func getAttribute(node *html.Node, name string) string {
-	for _, attribute := range node.Attr {
-		if attribute.Key == name {
-			return attribute.Val
+func getAttribute(node *lhtml.HtmlNode, name string) string {
+	for _, attribute := range node.Attributes {
+		if attribute.Name == name {
+			return attribute.Value
 		}
 	}
 	return ""
